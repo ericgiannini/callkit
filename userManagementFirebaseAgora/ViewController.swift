137f2d266005a1ba2e448800bec9b28ef4fdbc10
Floyd 2001 <floyd2001@floyds-macbook-pro.local>
2019-03-05 14:14:59 -0800

    Initial Commit

---
+++ userManagementFirebaseAgora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  userManagementFirebaseAgora
+//
+//  Created by Floyd 2001 on 3/5/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
